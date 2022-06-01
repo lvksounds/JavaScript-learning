8cee1d5de3b835ca669ccc57dd7ae8e79672cf02
lucas.freitas <lvksounds@gmail.com>
2022-06-01 15:26:16 -0300

    att 01/06

--- Estudos/objetos/testeObjetos.js
+++ Estudos/objetos/testeObjetos.js
@@ -10,10 +10,28 @@ let pessoa = {
         console.log(this.nome[0] + ' ' + this.nome[1] + ' tem ' + this.idade + ' anos de idade. Ele gosta de ' + this.interesses[0] + ' e ' + this.interesses[1] + '.');
     },
     saudacao: function(){
-        console.log('Oi! Eu sou ' + this.nome[0] + '.')
+        console.log('Oi! Eu sou ' + this.nome[0] + '.');
     }
 };
 
-console.log(pessoa.nome.primeiro)
 // console.log(pessoa.idade)
 // console.log(pessoa.bio())
+// pessoa['olhos'] = 'castanho escuro'
+// pessoa['despedida'] = function(){console.log("Adeus meros mortais!")}
+// console.log(pessoa.despedida())
+
+function pegaNome(){
+    let nome = document.getElementById('nome').value;
+    let txt = document.getElementById('texto');
+    let dados = document.getElementById('dados');
+    let botoes = document.getElementById('botoes')
+
+    if(nome == pessoa['nome']['primeiro']){
+        txt.innerHTML = `<h1>Olá ${nome}. Voce confirma que esses dados remetem a você?`
+        dados.innerHTML = `nome: ${pessoa.nome.primeiro}, idade: ${pessoa.idade}, sexo: ${pessoa.sexo} <br> interesses: ${pessoa.interesses[0]} e ${pessoa.interesses[1]}`
+    }
+
+    if (txt.value != ' '){
+        botoes.style.display = 'flex'
+    }
+}
